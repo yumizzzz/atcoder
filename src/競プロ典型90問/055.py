6b50fff84abeffaa5d4682f157a22d68ad226595
yumizu <yumizzds@gmail.com>
2024-03-29 15:46:14 +0000

    コード修正

--- src/競プロ典型90問/055.py
+++ src/競プロ典型90問/055.py
@@ -3,11 +3,9 @@ from itertools import combinations
 N, P, Q = map(int, input().split())
 A = list(map(int, input().split()))
 
-new_A = [a % P for a in A]
-
 ans = 0
-for a, b, c, d, e in combinations(new_A, 5):
-    if (a * b * c * d * e) % P == Q:
+for a, b, c, d, e in combinations(A, 5):
+    if a % P * b % P * c % P * d % P * e % P == Q:
         ans += 1
 
 print(ans)
