30356ffc23c2471905c21e295ba50976addbc8f5
yumizu <yumizzds@gmail.com>
2024-04-10 05:28:47 +0000

    解いた問題追加

--- src/atcoder過去問/abc332/b.py
+++ src/atcoder過去問/abc332/b.py
@@ -1,17 +1,17 @@
 K, G, M = map(int, input().split())
 
-g = 0
-m = 0
-for i in range(K):
+g, m = 0, 0
+
+for _ in range(K):
     if g == G:
         g = 0
     elif m == 0:
         m = M
     elif G - g < m:
-        m = m - (G - g)
+        m -= G - g
         g = G
     else:
-        g = g + m
+        g += m
         m = 0
 
 print(g, m)
--- src/atcoder過去問/abc333/b.py
+++ src/atcoder過去問/abc333/b.py
@@ -7,3 +7,12 @@ if (S in Z) == (T in Z):
     print("Yes")
 else:
     print("No")
+S = str(input())
+T = str(input())
+
+Z = "ABCDEAEDCBA"
+
+if (S in Z) == (T in Z):
+    print("Yes")
+else:
+    print("No")
